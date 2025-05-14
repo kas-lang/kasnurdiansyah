c17ed32929b307ce32a8f4f85ad3b7ef7805ca18
Kas Nur Diansyah <kasnurdiansyah@gmail.com>
2025-05-14 16:03:02 +0800

    Tugas 6

--- main.js
+++ main.js
@@ -37,48 +37,32 @@ const projects = [];
       }
 
       function renderCards() {
-        const container = document.getElementById("flexbox");
-        container.innerHTML = "";
-
-        projects.forEach((p) => {
-          flexbox.innerHTML += `
-        <div class="col-md-3 project-card">
-            <div class="card">
-                <img src="${p.img}" class="card-img-top" alt="${p.name}"/>
-                <div class="card-body">
-                    <h5 class="card-title">${p.name} - ${new Date().getFullYear()}</h5>
-                    <p class="card-text">durasi: ${p.duration} bulan</p>
-                    <p class="card-text">${p.desc}</p>
-                    <div>
-                    ${
-                      p.techs.node
-                        ? '<img class="tech-icon" src="assets/node.png" />'
-                        : ""
-                    }
-                    ${
-                      p.techs.react
-                        ? '<img class="tech-icon" src="assets/react.png" />'
-                        : ""
-                    }
-                    ${
-                      p.techs.next
-                        ? '<img class="tech-icon" src="assets/next.png" />'
-                        : ""
-                    }
-                    ${
-                      p.techs.typescript
-                        ? '<img class="tech-icon" src="assets/typescript.png" />'
-                        : ""
-                    }
-
-                    <div class=buttonmain>
-                    <button class="btn btn-secondary">Edit</button>
-                    <button class="btn btn-danger">Delete</button>
-                </div>
-                </div>
+  const container = document.getElementById("flexbox");
+  container.innerHTML ="";
+  
+  container.innerHTML = projects.map((p) => {
+    return `
+      <div class="col-md-3 project-card">
+        <div class="card">
+          <img src="${p.img}" class="card-img-top" alt="${p.name}"/>
+          <div class="card-body">
+            <h5 class="card-title">${p.name} - ${new Date().getFullYear()}</h5>
+            <p class="card-text">durasi: ${p.duration} bulan</p>
+            <p class="card-text">${p.desc}</p>
+            <div>
+              ${p.techs.node ? '<img class="tech-icon" src="assets/node.png" />' : ''}
+              ${p.techs.react ? '<img class="tech-icon" src="assets/react.png" />' : ''}
+              ${p.techs.next ? '<img class="tech-icon" src="assets/next.png" />' : ''}
+              ${p.techs.typescript ? '<img class="tech-icon" src="assets/typescript.png" />' : ''}
             </div>
+            <div class="buttonmain">
+              <button class="btn btn-secondary">Edit</button>
+              <button class="btn btn-danger">Delete</button>
+            </div>
+          </div>
         </div>
-        `;
-        });
-      }
+      </div>
+    `;
+  }).join("");
+}
         
